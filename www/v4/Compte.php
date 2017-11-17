bea26970b5b9d4b0c4b694a7362fd219667899d2
Tweety63 <32924326+Tweety63@users.noreply.github.com>
2017-11-17 17:28:07 +0100

    JC - page compte et index + php

---
+++ www/v4/Compte.php
@@ -0,0 +1,136 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN""http://www.w3c.org/TR/xhtml1/DTD/xtmlhtml1-strict.dtd">
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<link rel="icon" type="image/png" href="Images\iconeupop.png"/>
+		<!-- feuilles de style -->
+		<link rel="stylesheet" href="./css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="./css/main.css"/>
+		<!-- fichiers javascript -->
+		<script type="application/javascript" src="./js/jquery-3.2.1.min.js"></script>
+		<script type="application/javascript" src="./js/bootstrap.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>U POP</title>
+	</head>
+	<body>
+<!-- barre de navigation -->
+	<nav class="navbar navbar-default navbar-fixed-top menu">
+		<div class="container-fluid">
+
+			<div class="nav-header">
+				<button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" name="button">
+				 	<img class="imgButton" src="images/iconeupop.png">
+			 	</button>
+			</div>
+
+			<div class="collapse navbar-collapse navbar-left" id="menu">
+				<ul class="nav navbar-nav text-center">
+					<li><img class="logoBar" src="images/iconeupop.png"></li>
+					<li><a href="accueil.html" ><img class="imgButton" src="images/home.png"></a></li>
+					<li><a href="figurines.html"><img class="imgButton" src="images/figurine.png"></a></li>
+                    <!-- <li><h2 id="titre">Accueil</h2></li> -->
+					<li><a href="contact.html"><img class="imgButton" src="images/contact.png"></a></li>
+					<li><a href="compte.html"><img class="imgButton" src="images/compte.png"></a></li>
+					<li><a href="panier.html"><img class="imgButton" src="images/panier.png"></a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+<!-- fin barre de navigation -->
+		<!-- contenu de la page -->
+		<div class="container">
+			<div class="row centered-form">
+				<div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Compte de <?php echo $_SESSION['loginUser']; ?></h3>
+						</div>
+						<div class="panel-body">
+							<form role="form" method="post" action="/php/editCompte.php">
+								<div class="col-xs-12 col-sm-12 col-md-12">
+									<div class="form-group">
+										<input type="text" name="nomUser" id="nomUser" class="form-control input-sm" value=<?php echo $_SESSION['nomUser']; ?>>
+									</div>
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-12">
+									<div class="form-group">
+										<input type="text" name="prenomUser" id="prenomUser" class="form-control input-sm" value=<?php echo $_SESSION['prenomUser']; ?>>
+									</div>
+								</div>
+								<div class="col-xs-6 col-sm-6 col-md-6">
+									<div class="form-group">
+										<select name="genreUser" id="genreUser" class="form-control input-sm" value=<?php echo $_SESSION['genreUser']; ?>>
+											<option value="M">Masculin</option>
+											<option value="F">Féminin</option>
+										</select>
+									</div>
+								</div>
+								<div class="col-xs-6 col-sm-6 col-md-6">
+									<div class="form-group">
+										<input type="date" name="dateNaissanceUser" id="dateNaissanceUser" class="form-control input-sm" value=<?php echo $_SESSION['dateNaissanceUser']; ?>>
+									</div>
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-12">
+									<div class="form-group">
+										<input type="text" name="adresseUser" id="adresseUser" class="form-control input-sm" value=<?php echo $_SESSION['adresseUser']; ?>>
+									</div>
+								</div>
+								<div class="col-xs-6 col-sm-6 col-md-6">
+									<div class="form-group">
+										<input type="number" maxlength="5" name="cpUser" id="cpUser" class="form-control input-sm" value=<?php echo $_SESSION['cpUser']; ?>>
+									</div>
+								</div>
+								<div class="col-xs-6 col-sm-6 col-md-6">
+									<div class="form-group">
+										<input type="text" name="villeUser" id="villeUser" class="form-control input-sm" value=<?php echo $_SESSION['villeUser']; ?>>
+									</div>
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-12">
+									<div class="form-group">
+										<input type="email" name="mailUser" id="mailUser" class="form-control input-sm" value=<?php echo $_SESSION['mailUser']; ?>>
+									</div>
+								</div>
+								<button class="btn" type="submit">Valider</button>				
+							</form>
+						</div>
+					</div>
+                </div>
+                <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Changement de mot de passe</h3>
+						</div>
+						<div class="panel-body">
+							<form role="form">
+								<div class="col-xs-12 col-sm-12 col-md-12">
+									<div class="form-group">
+										<input type="password" name="passwordUser" id="passwordUser" class="form-control input-sm" value=<?php echo $_SESSION['passwordUser']; ?>>
+									</div>
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-12">
+									<div class="form-group">
+										<input type="password" name="passwordUser2" id="passwordUser2" class="form-control input-sm" value=<?php echo $_SESSION['passwordUser']; ?>>
+									</div>
+                                </div>
+                                <button class="btn" type="submit">Valider</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+			</div>
+		</div>
+	</div>
+	<!-- fin articles de la page -->
+</div>
+<!-- fin contenu de la page -->
+<script>
+
+
+</script>
+
+</body>
+</html>
--- www/v4/php/connexion.php
+++ www/v4/php/connexion.php
@@ -4,18 +4,18 @@ Page: connexion.php
 */
 session_start(); // à mettre tout en haut du fichier .php, cette fonction propre à PHP servira à maintenir la $_SESSION
 if(isset($_POST['connexion'])) { // si le bouton "Connexion" est appuyé
-    // on vérifie que le champ "userName" n'est pas vide
+    // on vérifie que le champ "loginUser" n'est pas vide
     // empty vérifie à la fois si le champ est vide et si le champ existe belle et bien (is set)
-    if(empty($_POST['userName'])) {
-        echo "Le champ userName est vide.";
+    if(empty($_POST['loginUser'])) {
+        echo "Le champ loginUser est vide.";
     } else {
         // on vérifie maintenant si le champ "Mot de passe" n'est pas vide"
-        if(empty($_POST['userPassword'])) {
+        if(empty($_POST['passwordUser'])) {
             echo "Le champ Mot de passe est vide.";
         } else {
             // les champs sont bien posté et pas vide, on sécurise les données entrées par le membre:
-            $userName = htmlentities($_POST['userName'], ENT_QUOTES, "ISO-8859-1"); // le htmlentities() passera les guillemets en entités HTML, ce qui empêchera les injections SQL
-            $MotDePasse = htmlentities($_POST['userPassword'], ENT_QUOTES, "ISO-8859-1");
+            $loginUser = htmlentities($_POST['loginUser'], ENT_QUOTES, "ISO-8859-1"); // le htmlentities() passera les guillemets en entités HTML, ce qui empêchera les injections SQL
+            $passwordUser = htmlentities($_POST['passwordUser'], ENT_QUOTES, "ISO-8859-1");
             //on se connecte à la base de données:
             $mysqli = mysqli_connect("localhost", "root", "root", "uPop");
             //on vérifie que la connexion s'effectue correctement:
@@ -23,15 +23,26 @@ if(isset($_POST['connexion'])) { // si le bouton "Connexion" est appuyé
                 echo "Erreur de connexion à la base de données.";
             } else {
                 // on fait maintenant la requête dans la base de données pour rechercher si ces données existe et correspondent:
-                $Requete = mysqli_query($mysqli,"SELECT * FROM FICHE_USER WHERE loginUser = '".$userName."' AND passwordUser = '".$MotDePasse."'");
+                $Requete = mysqli_query($mysqli,"SELECT * FROM FICHE_USER WHERE loginUser = '".$loginUser."' AND passwordUser = '".$passwordUser."'");
                 // si il y a un résultat, mysqli_num_rows() nous donnera alors 1
                 // si mysqli_num_rows() retourne 0 c'est qu'il a trouvé aucun résultat
                 if(mysqli_num_rows($Requete) == 0) {
-                    echo "Le userName ou le mot de passe est incorrect, le compte n'a pas été trouvé.";
+                    echo "Le loginUser ou le mot de passe est incorrect, le compte n'a pas été trouvé.";
                 } else {
                     // on ouvre la session avec $_SESSION:
-                    $_SESSION['userName'] = $userName; // la session peut être appelée différemment et son contenu aussi peut être autre chose que le userName
+                    $_SESSION['loginUser'] = $loginUser; // la session peut être appelée différemment et son contenu aussi peut être autre chose que le loginUser
                     //echo "Vous êtes à présent connecté !";
+                    $_SESSION['passwordUser'] = $passwordUser;
+                    $Row = mysqli_fetch_row($Requete);
+                    $_SESSION['nomUser'] = $Row[1];
+                    $_SESSION['prenomUser'] = $Row[2];
+                    $_SESSION['genreUser'] = $Row[3];
+                    $_SESSION['dateNaissanceUser'] = $Row[4];
+                    $_SESSION['adresseUser'] = $Row[6];
+                    $_SESSION['cpUser'] = $Row[7];
+                    $_SESSION['villeUser'] = $Row[8];
+                    $_SESSION['mailUser'] = $Row[9];
+                    
                     header("Location: ../Accueil.html"); // Redirection du navigateur
                     exit;
                 }
