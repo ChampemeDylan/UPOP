51b1a000a169e1fda2ed86f7ce4fc15b36673fd8
DESKTOP-2DQ70S0\etudiant20 <lebot.ken@laposte.net>
2017-11-22 17:24:35 +0100

    Ken Ajout page admin en php + modifElement avec session
    Version test page admin avec recuperation des donnees + gestion session

--- www/v4/php/modifElement.php
+++ www/v4/php/modifElement.php
@@ -1,4 +1,12 @@
 <?php
+
+$loginUser = $_SESSION['loginUser'];
+$refArticle = htmlentities($_POST['refArticle'], ENT_QUOTES, "ISO-8859-1");
+$libelle = htmlentities($_POST['libelleArticle'], ENT_QUOTES, "ISO-8859-1");
+$description = htmlentities($_POST['descriptifArticle'], ENT_QUOTES, "ISO-8859-1");
+$prix = htmlentities($_POST['prixArticle'], ENT_QUOTES, "ISO-8859-1");
+$univers = htmlentities($_POST['libelleUnivers'], ENT_QUOTES, "ISO-8859-1");
+
 try
 {
 	//on se connecte à la base de données
@@ -15,18 +23,25 @@ catch(Exception $e)
 }
 
 $mysqli = mysqli_connect("localhost", "root", "user", "Upop");
-$reference = $_POST['Reference'];
+$refArticle = $_POST['refArticle'];
 
-if(empty($_POST['Reference'])) {
+if(empty($_POST['refArticle'])) {
 	echo "La reference est vide.";
 } else {
-	$Requete = mysqli_query($mysqli,"SELECT * FROM UNIVERS WHERE refArticle = '".$reference."'");
-				// si il y a un résultat, mysqli_num_rows() nous donnera alors 1
-				// si mysqli_num_rows() retourne 0 c'est qu'il a trouvé aucun résultat
-				if(mysqli_num_rows($Requete) == 0) {
-					echo "Cette reference n'existe pas.";
-				} else {
-					// On récupère le contenu de la table qui correspond a la reference
-					$reponse = $bdd->query('SELECT libelleArticle, descriptionArticle, prixArticle, libelleUnivers FROM FICHE_ARTICLE WHERE refArticle = "'.$reference.'"');
-				}
+	$Requete = mysqli_query($mysqli,"SELECT * FROM UNIVERS WHERE refArticle = '".$refArticle."'");
+		// si il y a un résultat, mysqli_num_rows() nous donnera alors 1
+		// si mysqli_num_rows() retourne 0 c'est qu'il a trouvé aucun résultat
+		if(mysqli_num_rows($Requete) == 0) {
+			echo "Cette reference n'existe pas.";
+		} else {
+			// On récupère le contenu de la table qui correspond a la reference
+			$reponse = $bdd->query('SELECT libelleArticle, descriptifArticle, prixArticle, libelleUnivers FROM FICHE_ARTICLE WHERE refArticle = "'.$refArticle.'"');
+		}
+		
+		// mise a jour
+		$SESSION['refArticle'] = $refArticle;
+		$SESSION['libelleArticle'] = $libelle;
+		$SESSION['descriptifArticle'] = $description;
+		$SESSION['prixArticle'] = $prix;
+		$SESSION['libelleUnivers'] = $univers;
 }
