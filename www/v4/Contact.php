0c4071e1696804743cf67dabde9083d9869e6b17
DESKTOP-EF0SEA4\etudiant15 <anis.laraba@gmx.com>
2017-11-22 15:46:51 +0100

    Anis: Page Contact.php
    Changement de l'adresse mail.
    Nettoyage + Commentaires.

--- www/v4/Contact.php
+++ www/v4/Contact.php
@@ -1,26 +1,3 @@
-
-<!-- à mettre dans le fichier php.ini de Wamp ligne 1026 -->
-
-<!-- [mail function]
-; For Win32 only.
-; http://php.net/smtp
-SMTP = smtp.gmail.com
-; http://php.net/smtp-port
-smtp_port = 465
-
-; For Win32 only.
-; http://php.net/sendmail-from
-sendmail_from ="udevpromo2017@gmail.com"
-
-; For Unix only.  You may supply arguments as well (default: "sendmail -t -i").
-; http://php.net/sendmail-path
-sendmail_path = "C:\Wamp64\sendmail\sendmail.exe -t - -->
-
-
-<!-- Mettre le dossier sendmail dans Wamp64 -->
-
-
-
 <?php
 
 // Ouverture de session
@@ -28,34 +5,23 @@ sendmail_path = "C:\Wamp64\sendmail\sendmail.exe -t - -->
 session_start();
 
 require "./php/verifConnexion.php";
-/*
-	********************************************************************************************
-	CONFIGURATION
-	********************************************************************************************
-*/
-// destinataire est votre adresse mail. Pour envoyer à plusieurs à la fois, séparez-les par une virgule
-$destinataire = 'udevpromo2017@gmail.com';
 
-// copie ? (envoie une copie au visiteur)
+//	Configuration
+
+// destinataire (si plusieur destinataire séparer avec des virgules)
+$destinataire = 'upop.contact@gmail.com';
+
+// copie envoyée à l'éxpediteur
 $copie = 'oui';
 
-// Action du formulaire (si la page a des paramètres dans l'URL)
-// si cette page est index.php?page=contact alors mettez index.php?page=contact
-// sinon, faut laisser vide
 $form_action = '';
 
-// Messages de confirmation du mail
-$message_envoye = "Votre message nous est bien parvenu !";
+// Messages de confirmation ou échec du mail
+$message_envoye = "Votre messagea été bien envoyé !";
 $message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
 
 // Message d'erreur du formulaire
-$message_formulaire_invalide = "Vérifiez que tous les champs soient bien remplis et que l'email soit sans erreur.";
-
-/*
-	********************************************************************************************
-	FIN DE LA CONFIGURATION
-	********************************************************************************************
-*/
+$message_formulaire_invalide = "Vérifiez que tous les champs soient bien remplis.";
 
 /*
  * cette fonction sert à nettoyer et enregistrer un texte
@@ -89,192 +55,187 @@ $message = (isset($_POST['message'])) ? Rec($_POST['message']) : '';
 
 // On  vérifie les variables et l'email ...
 $email = (IsEmail($email)) ? $email : ''; // soit l'email est vide si erroné, soit il vaut l'email entré
-$err_formulaire = false; // sert pour remplir le formulaire en cas d'erreur si besoin
+$err_formulaire = false; // sert à remplir le formulaire en cas d'erreur si besoin
 
-if (isset($_POST['envoi']))
-{
-	if (($nom != '') && ($email != '') && ($objet != '') && ($message != ''))
-	{
-		// les 4 variables sont remplies, on génère puis envoie le mail
-		$headers  = 'MIME-Version: 1.0' . "\r\n";
-		$headers .= 'From:'.$nom.' <'.$email.'>' . "\r\n" .
-				'Reply-To:'.$email. "\r\n" .
-				'Content-Type: text/plain; charset="utf-8"; DelSp="Yes"; format=flowed '."\r\n" .
-				'Content-Disposition: inline'. "\r\n" .
-				'Content-Transfer-Encoding: 7bit'." \r\n" .
-				'X-Mailer:PHP/'.phpversion();
-
-		// envoyer une copie au visiteur ?
-		if ($copie == 'oui')
-		{
-			$cible = $destinataire.';'.$email;
-		}
-		else
-		{
-			$cible = $destinataire;
-		};
-
-		// Remplacement de certains caractères spéciaux
-		$message = str_replace("&#039;","'",$message);
-		$message = str_replace("&#8217;","'",$message);
-		$message = str_replace("&quot;",'"',$message);
-		$message = str_replace('<br>','',$message);
-		$message = str_replace('<br />','',$message);
-		$message = str_replace("&lt;","<",$message);
-		$message = str_replace("&gt;",">",$message);
-		$message = str_replace("&amp;","&",$message);
-
-		// Envoi du mail
-		$num_emails = 0;
-		$tmp = explode(';', $cible);
-		foreach($tmp as $email_destinataire)
-		{
-			if (mail($email_destinataire, $objet, $message, $headers))
-				$num_emails++;
-		}
-
-		if ((($copie == 'oui') && ($num_emails == 2)) || (($copie == 'non') && ($num_emails == 1)))
-		{
-			echo '<p>'.$message_envoye.'</p>';
-		}
-		else
-		{
-			echo '<p>'.$message_non_envoye.'</p>';
-		};
-	}
-	else
-	{
-		// une des 3 variables (ou plus) est vide ...
-		echo '<p>'.$message_formulaire_invalide.'</p>';
-		$err_formulaire = true;
-	};
-}; // fin du if (!isset($_POST['envoi']))
-
-if (($err_formulaire) || (!isset($_POST['envoi'])))
-{
-	
-};
 ?>
 
-
-
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN""http://www.w3c.org/TR/xhtml1/DTD/xtmlhtml1-strict.dtd">
 <html>
-	<head>
+<head>
 
-		
-		<link rel="icon" type="image/png" href="Images\iconeupop.png"/>
-		<!-- feuilles de style -->
-		<link rel="stylesheet" href="./css/bootstrap.min.css"/>
-		<link rel="stylesheet" href="./css/main.css"/>
-		<link rel="stylesheet" href="./css/contact.css">
-		<!-- fichiers javascript -->
-		<script type="application/javascript" src="./js/jquery-3.1.1.min.js"></script>
-		<script type="application/javascript" src="./js/bootstrap.min.js"></script>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="icon" type="image/png" href="Images\iconeupop.png"/>
+	<!-- feuilles de style -->
+	<link rel="stylesheet" href="./css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="./css/main.css"/>
+	<link rel="stylesheet" href="./css/contact.css">
+	<!-- fichiers javascript -->
+	<script type="application/javascript" src="./js/jquery-3.2.1.min.js"></script>
+	<script type="application/javascript" src="./js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
 
-		<title>U POP</title>
+	<title>U POP</title>
 	
-	</head>
-	<body>
-<!-- barre de navigation -->
+</head>
+<body>
+	<!-- barre de navigation -->
 	<nav class="navbar navbar-default navbar-fixed-top menu">
-        <div class="container-fluid">
-            <div class="nav-header">
-                <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" name="button">
-                    <img class="imgButton" src="images/iconeupop.png">
-                </button>
-            </div>
+		<div class="container-fluid">
+			<div class="nav-header">
+				<button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" name="button">
+					<img class="imgButton" src="images/iconeupop.png">
+				</button>
+			</div>
 
-            <div class="collapse navbar-collapse" id="menu">
-                <ul class="nav navbar-nav">
-                    <li><img class="logoBar" src="images/iconeupop.png"></li>
-                    <li><a href="accueil.php" ><img class="imgButton" src="images/home.png"></a></li>
-                    <li><a href="figurines.php"><img class="imgButton" src="images/figurine.png"></a></li>
-                    <!-- <li><h2 id="titre">Accueil</h2></li> -->
-                    <li><a href="contact.php"><img class="imgButton" src="images/contact.png"></a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                	<li><a href="compte.php"><img class="imgButton" src="images/compte.png"><?php echo ' '.$_SESSION['loginUser']?></a></li>
-                	<li><a href="panier.php"><img class="imgButton" src="images/panier.png"></a></li>
-                    <li><a href="php/deco.php"><img class="imgButton" src="images/deco.png"></a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+			<div class="collapse navbar-collapse" id="menu">
+				<ul class="nav navbar-nav">
+					<li><img class="logoBar" src="images/iconeupop.png"></li>
+					<li><a href="accueil.php" ><img class="imgButton" src="images/home.png"></a></li>
+					<li><a href="figurines.php"><img class="imgButton" src="images/figurine.png"></a></li>
+					<!-- <li><h2 id="titre">Accueil</h2></li> -->
+					<li><a href="contact.php"><img class="imgButton" src="images/contact.png"></a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="compte.php"><img class="imgButton" src="images/compte.png"><?php echo ' '.$_SESSION['loginUser']?></a></li>
+					<li><a href="panier.php"><img class="imgButton" src="images/panier.png"></a></li>
+					<li><a href="php/deco.php"><img class="imgButton" src="images/deco.png"></a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 
 	
-<!-- Formulaire à envoyer -->			
-			<form id="contact" method="post" action="<?php echo ' '.$form_action?>"> 
-				<div class="container">
-					<div class="row">
-						<div class="col-md-8">
-							<div class="well well-sm panel panel-default test">
-								<form>
-									<div class="row">
-										<div class="col-md-6">
-											<div class="form-group">	
-																																		
-												<fieldset><p><label for="nom ">Nom : </label> <input type="text" id="nom" name="nom" value="<?php echo ' '.stripslashes($nom)?>" tabindex="1" /></p>
-													<p>
-														<div class="input-group">
-															<span class="input-group-addon">
-																<span class="glyphicon glyphicon-envelope">
-																</span>
-															</span>
-																<input type="email" class="form-control" id="email" placeholder="E-mail" name="email" value="<?php echo ' '.stripslashes($email)?>" tabindex="2" />
-														</div>
-													</p>
-												</fieldset>
+	<!-- Formulaire à envoyer -->			
+	<form id="contact" method="post" action="<?php echo ' '.$form_action?>"> 
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
+					<div class="well well-sm panel panel-default test">
+						<form>
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">	
+										<!-- Variable qui récupère le nom -->																																		
+										<fieldset><p><label for="nom ">Nom : </label> <input type="text" id="nom" name="nom" value="<?php echo ' '.stripslashes($nom)?>" tabindex="1" /></p>
+											<p>
+												<div class="input-group">
+													<span class="input-group-addon">
+														<span class="glyphicon glyphicon-envelope">
+														</span>
+													</span>
+													<!-- Variable qui récupère le mail -->															
+													<input type="email" class="form-control" id="email" placeholder="E-mail" name="email" value="<?php echo ' '.stripslashes($email)?>" tabindex="2" />
+												</div>
+											</p>
+										</fieldset>
 
-												
-												<p><label for="objet">Objet :</label> <input type="text" id="objet" name="objet" value="<?php echo ' '.stripslashes($objet)?>" tabindex="3" />
-												</p>
+
+										<p><label for="objet">Objet :</label> <input type="text" id="objet" name="objet" value="<?php echo ' '.stripslashes($objet)?>" tabindex="3" />
+										</p>
 										<p>
-
+											<!-- Variable qui récupère le texte -->
 											<fieldset><legend>Votre message :</legend>
 
-											<textarea id="message" name="message" tabindex="4" cols="60" rows="4"><?php echo ' '.stripslashes($message)?></textarea>
-										</p>
-											</fieldset>
+												<textarea id="message" name="message" tabindex="4" " rows="4"><?php echo ' '.stripslashes($message)?></textarea>
+											</p>
+										</fieldset>
+									</div>     
 
-														
-			
 
-											</div>
-													</div>
-														<div class="col-md-12">
-																<div style="text-align:center;"><button type="submit" name="envoi" value="Envoyer" class="btn pull-right" id="btnContactUs">Envoyer</button>
+												<span class="erreurEnvoie">  
 
-																</div>
-														</div>
-													</div>
-								</form>
-						</div>
-					</div>
-					<div class="well well-sm panel panel-default test col-md-4">
-						<address>
-							<strong>UPOP</strong>
-							<br>
-							107 rue de lUDEV
-							<br>
-							63000 CLERMONT-FERRAND
-							<br>
-							Tél :
-							<abbr title="Phone">
-							(+33) 9 00 10 12 13
-							</abbr>
-						</address>
+																<?php	if (isset($_POST['envoi'])){
+																		if (($nom != '') && ($email != '') && ($objet != '') && ($message != '')){
+// Les 4 variables sont remplies, on génère puis l'envoie le mail
+																		$headers  = 'MIME-Version: 1.0' . "\r\n";
+																		$headers .= 'From:'.$nom.' <'.$email.'>' . "\r\n" .
+																					'Reply-To:'.$email. "\r\n" .
+																					'Content-Type: text/plain; charset="utf-8"; DelSp="Yes"; format=flowed '."\r\n" .
+																					'Content-Disposition: inline'. "\r\n" .
+																					'Content-Transfer-Encoding: 7bit'." \r\n" .
+																					'X-Mailer:PHP/'.phpversion();
 
-						<address>
-							<strong>Mail-contact</strong>
-							<br>
-							<a href="mailto:#">upop.contact@gmail.com</a>
-						</address>
+// L'envoie d'une copie à l'éxpediteur
+																		if ($copie == 'oui')
+																		{
+																			$cible = $destinataire.';'.$email;
+																		}
+																		else
+																		{
+																			$cible = $destinataire;
+																		};
+
+// Remplacement de certains caractères spéciaux
+																		$message = str_replace("&#039;","'",$message);
+																		$message = str_replace("&#8217;","'",$message);
+																		$message = str_replace("&quot;",'"',$message);
+																		$message = str_replace('<br>','',$message);
+																		$message = str_replace('<br />','',$message);
+																		$message = str_replace("&lt;","<",$message);
+																		$message = str_replace("&gt;",">",$message);
+																		$message = str_replace("&amp;","&",$message);
+
+// Envoi du mail
+																		$num_emails = 0;
+																		$tmp = explode(';', $cible);
+																		foreach($tmp as $email_destinataire)
+																		{
+																			if (mail($email_destinataire, $objet, $message, $headers))
+																				$num_emails++;
+																		}
+
+																		if ((($copie == 'oui') && ($num_emails == 2)) || (($copie == 'non') && ($num_emails == 1))){
+																			echo '<p>'.$message_envoye.'</p>';
+																		}else{
+																			echo '<p>'.$message_non_envoye.'</p>';
+																		};
+
+																		}else{
+// Si une des 3 variables (ou plus) est vide --> Erreur
+																		echo '<p>'.$message_formulaire_invalide.'</p>';
+																		$err_formulaire 
+																		= true;
+																		};
+																		}; 
+
+																	?>
+												</span>
+								</div>
+								<div class="col-md-12">
+<!-- Bouton d'envoi du formulaires -->
+									<div style="text-align:center;"><input type="submit" name="envoi" value="Envoyer" class="btn pull-right" id="btnContactUs">
+
+									</div>
+								</div>
+							</div>
+						</form>
+						<div class="erreurEvnoie">
+						</div>								
 					</div>
 				</div>
+				<div class="well well-sm panel panel-default test col-md-4">
+					<address>
+						<strong>UPOP</strong>
+						<br>
+						107 rue de lUDEV
+						<br>
+						63000 CLERMONT-FERRAND
+						<br>
+						Tél :
+						<abbr title="Phone">
+							(+33) 9 00 10 12 13
+						</abbr>
+					</address>
+
+					<address>
+						<strong>Mail-contact</strong>
+						<br>
+						<a href="mailto:#">upop.contact@gmail.com</a>
+					</address>
+				</div>
 			</div>
-		</form>
-	</body>
+		</div>
+	</form>
+</body>
 </html>
