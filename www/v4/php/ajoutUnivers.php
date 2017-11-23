9574fed4570b8c78a5fc88d3e4abd9f66b736c7f
DESKTOP-2DQ70S0\etudiant20 <lebot.ken@laposte.net>
2017-11-23 17:43:05 +0100

    Ken : uniformisation des fonction php + ajout suprUnivers

--- www/v4/php/ajoutUnivers.php
+++ www/v4/php/ajoutUnivers.php
@@ -3,7 +3,7 @@ try
 {
 	//on se connecte à la base de données
 	// en local
-	$bdd = new PDO('mysql:host=localhost;dbname=uPop;charset=utf8', 'root', 'root');
+	$bdd = new PDO('mysql:host=localhost;dbname=uPop;charset=utf8', 'root', '');
 	
 	//en online
 	//$bdd = new PDO('mysql:host=db708219960.db.1and1.com;dbname=db708219960', 'dbo708219960', 'dbo708219960');
@@ -14,35 +14,75 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 // On associe la valeur de l'input à la variable
-$univers = $_POST['Univers'];
-$mysqli = mysqli_connect("localhost", "root", "user", "Upop");
+$libelleUnivers = $_POST['libelleUnivers'];
+//$mysqli = mysqli_connect("localhost", "root", "user", "Upop");
 $categ1 = 'Jeux vidéos';
 $categ2 = 'Séries';
 $categ3 = 'Films';
 $categ4 = 'Animés';
+
 // On verifie si les données non nulles ne sont pas vides
 
-if(empty($_POST['Univers'])) {
-	echo "L'univers est vide.";
+if(empty($_POST['libelleUnivers'])) {
+	//echo "L'univers est vide.";
+	header("Location: ../administration.php?erreurlibelle2=bad_lib2");
 } else {
-	$Requete = mysqli_query($mysqli,"SELECT * FROM UNIVERS WHERE libelleUnivers = '".$univers."'");
+	//$Requete = mysqli_query($mysqli,"SELECT * FROM UNIVERS WHERE libelleUnivers = '".$libelleUnivers."'");
+	$sql = "SELECT * FROM UNIVERS WHERE libelleUnivers = :libelleUnivers";
+	$stmt = $bdd->prepare($sql);
+	
+	$stmt->execute(array( 
+		'libelleUnivers' => $libelleUnivers
+	));
+	
 	// si il y a un résultat, mysqli_num_rows() nous donnera alors 1
 	// si mysqli_num_rows() retourne 0 c'est qu'il a trouvé aucun résultat
-	if(mysqli_num_rows($Requete) == 1) {
-		echo "Cet univers existe deja.";
+	//if(mysqli_num_rows($Requete) == 1) {
+	if($stmt->rowCount() == 1) {	
+		//echo "Cet univers existe deja.";
+		header("Location: ../administration.php?erreurexist2=bad_exist2");
 	} else {
-		$bdd->exec('INSERT INTO UNIVERS VALUES (\''.$univers.'\')');
-		if(isset($_POST['Categ1'])) {
-			$bdd->exec('INSERT INTO UNIVERS_CATEGORIE VALUES (\''.$univers.'\', \''.$categ1.'\')');
+		//$bdd->exec('INSERT INTO UNIVERS VALUES (\''.$libelleUnivers.'\')');
+		$sql = "INSERT INTO UNIVERS VALUES (:libelleUnivers)";
+		$stmt = $bdd->prepare($sql);
+		$stmt->execute(array(
+			'libelleUnivers' => $libelleUnivers
+		));
+		if(isset($_POST['categ1'])) {
+			//$bdd->exec('INSERT INTO UNIVERS_CATEGORIE VALUES (\''.$libelleUnivers.'\', \''.$categ1.'\')');
+			$sql = "INSERT INTO UNIVERS_CATEGORIE VALUES (:libelleUnivers, :categ1)";
+			$stmt = $bdd->prepare($sql);
+			$stmt->execute(array(
+				'libelleUnivers' => $libelleUnivers,
+				'categ1' => $categ1
+			));
 		}
-		if(isset($_POST['Categ2'])) {
-			$bdd->exec('INSERT INTO UNIVERS_CATEGORIE VALUES (\''.$univers.'\', \''.$categ2.'\')');
+		if(isset($_POST['categ2'])) {
+			//$bdd->exec('INSERT INTO UNIVERS_CATEGORIE VALUES (\''.$libelleUnivers.'\', \''.$categ2.'\')');
+			$sql = "INSERT INTO UNIVERS_CATEGORIE VALUES (:libelleUnivers, :categ2)";
+			$stmt = $bdd->prepare($sql);
+			$stmt->execute(array(
+				'libelleUnivers' => $libelleUnivers,
+				'categ2' => $categ2
+			));
 		}
-		if(isset($_POST['Categ3'])) {
-			$bdd->exec('INSERT INTO UNIVERS_CATEGORIE VALUES (\''.$univers.'\', \''.$categ3.'\')');
+		if(isset($_POST['categ3'])) {
+			//$bdd->exec('INSERT INTO UNIVERS_CATEGORIE VALUES (\''.$libelleUnivers.'\', \''.$categ3.'\')');
+			$sql = "INSERT INTO UNIVERS_CATEGORIE VALUES (:libelleUnivers, :categ3)";
+			$stmt = $bdd->prepare($sql);
+			$stmt->execute(array(
+				'libelleUnivers' => $libelleUnivers,
+				'categ3' => $categ3
+			));
 		}
-		if(isset($_POST['Categ4'])) {
-			$bdd->exec('INSERT INTO UNIVERS_CATEGORIE VALUES (\''.$univers.'\', \''.$categ4.'\')'); 
+		if(isset($_POST['categ4'])) {
+			//$bdd->exec('INSERT INTO UNIVERS_CATEGORIE VALUES (\''.$libelleUnivers.'\', \''.$categ4.'\')'); 
+			$sql = "INSERT INTO UNIVERS_CATEGORIE VALUES (:libelleUnivers, :categ4)";
+			$stmt = $bdd->prepare($sql);
+			$stmt->execute(array(
+				'libelleUnivers' => $libelleUnivers,
+				'categ4' => $categ4
+			));
 		}
 	}
 }
--- www/v4/php/supprimUnivers.php
+++ www/v4/php/supprimUnivers.php
@@ -15,47 +15,49 @@ catch(Exception $e)
 }
 // On associe la valeur de l'input à la variable
 //$mysqli = mysqli_connect("localhost", "root", "user", "Upop");
-$refArticle = $_POST['refArticle'];
-$libelleArticle = $_POST['libelleArticle'];
 $libelleUnivers = $_POST['libelleUnivers'];
 
 
 // On verifie si les données non nulles ne sont pas vides
 
-if(empty($_POST['refArticle'])) {
-	//echo "La reference est vide.";
-	header("Location: ../administration.php?erreurref2=bad_ref2");
+if(empty($_POST['libelleUnivers'])) {
+	//echo "L'univers est vide.";
+	header("Location: ../administration.php?erreurexist4=bad_exist4");
 } else {
-	if(empty($_POST['libelleArticle'])) {
-		//echo "Le libelle est vide.";
-		header("Location: ../administration.php?erreurlibelle3=bad_lib3");
-	} else {
 		//$Requete = mysqli_query($mysqli,"SELECT * FROM FICHE_ARTICLE WHERE libelleUnivers = '".$libelleUnivers."' AND refArticle = '".$refArticle."' AND libelleArticle = '".$libelleArticle."'");
-		$sql = "SELECT * FROM FICHE_ARTICLE WHERE libelleUnivers = :libelleUnivers AND refArticle = :refArticle AND libelleArticle = :libelleArticle";
+		$sql = "SELECT * FROM UNIVERS WHERE libelleUnivers = :libelleUnivers";
 		$stmt = $bdd->prepare($sql);
 		
 		$stmt->execute(array(
-			'libelleUnivers' => $libelleUnivers,
-			'refArticle' => $refArticle,
-			'libelleArticle' => $libelleArticle
+			'libelleUnivers' => $libelleUnivers
 		));
 		
 		// si il y a un résultat, mysqli_num_rows() nous donnera alors 1
 		// si mysqli_num_rows() retourne 0 c'est qu'il a trouvé aucun résultat
 		//if(mysqli_num_rows($Requete) == 1) {
 		if($stmt->rowcount() == 0){	
-			//echo "Cet element n'existe pas.";
-			header("Location: ../administration.php?erreurexist3=bad_exist3");
+			//echo "Cet univers n'existe pas.";
+			header("Location: ../administration.php?erreurexist5=bad_exist5");
 		} else {
 			//$bdd->exec('DELETE FROM FICHE_ARTICLE WHERE refArticle = "'.$refArticle.'"');
-			$sql = "DELETE FROM FICHE_ARTICLE WHERE refArticle = :refArticle";
+			$sql = "DELETE FROM UNIVERS WHERE libelleUnivers = :libelleUnivers";
 			$stmt = $bdd->prepare($sql);
 			$stmt->execute(array(
-				'refArticle' => $refArticle
+				'libelleUnivers' => $libelleUnivers
 			));
+			$sql = "DELETE FROM UNIVERS_CATEGORIE WHERE libelleUnivers = :libelleUnivers";
+			$stmt = $bdd->prepare($sql);
+			$stmt->execute(array(
+				'libelleUnivers' => $libelleUnivers
+			));
+			$sql = "DELETE FROM FICHE_ARTICLE WHERE libelleUnivers = :libelleUnivers";
+			$stmt = $bdd->prepare($sql);
+			$stmt->execute(array(
+				'libelleUnivers' => $libelleUnivers
+			));
+			echo "Univers supprimer";
 		}
 	}
-}
 
 
 ?>
