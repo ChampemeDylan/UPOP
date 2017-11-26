90018ab6890768246a3011b110dc200d7efe9488
DESKTOP-EF0SEA4\etudiant15 <anis.laraba@gmx.com>
2017-11-26 21:19:26 +0100

    Anis: validationCommande.php
    Mail Confirmation de Commande.

--- www/v4/php/validationCommande.php
+++ www/v4/php/validationCommande.php
@@ -1,5 +1,50 @@
 <?php
 session_start();
+
+//Récupération des vriables du formulaire :
+                $mailUser  = $_SESSION['mailUser'];
+                $loginUser = $_SESSION['loginUser'];
+                $numCommande = $_SESSION['numCommande'];
+
+                 
+// Mail :
+                $objet =    'Confirmation de votre commande UPop';
+                $contenu =  '<html>
+                                <head>
+                                    <div>
+                                        <span>
+                                            <img src="http://upop.champemedylan.fr/images/iconeupop.png" style="width: 140px;"></img>
+                                        </span>
+                                    </div>
+                                    
+                                    <hr>
+                                </head>
+                                    <body>
+                                        <p style="font-size: 22px;">    Bonjour '.$loginUser.',
+                                            Votre commande N°'.$numCommande.' a bien été prise en compte.
+                                            Merci de votre confiance et à bientôt sur Upop.
+                                        </p><br>
+                                        <p style="font-size: 22px;">
+                                            Cordialement l\'équipe U\'pop
+                                        </p>
+                                            <hr>
+                                        <p style="font-size: 12px;">
+                                            © 2017 Copyright: U\'Pop -- Ce site est un projet étudiant et non un site de vente
+                                        </p>
+
+                                    </body>
+                            </html>';
+
+//Configuration :
+                            
+                $headers  = 'MIME-Version: 1.0' . "\r\n";
+                $headers .= 'From:'.$loginUser.' <'.$mailUser.'>' . "\r\n" .
+                            'Reply-To:'.$mailUser. "\r\n" .
+                            'Content-Type: text/html; charset="utf-8"; DelSp="Yes"; format=flowed '."\r\n" .
+                            'Content-Disposition: inline'. "\r\n" .
+                            'Content-Transfer-Encoding: 8bit'." \r\n" .
+                            'X-Mailer:PHP/'.phpversion();
+
 	 //on se connecte à la base de données:
 try
 {
@@ -26,6 +71,9 @@ else
     $stmt = $bdd->prepare($sql);
     $stmt->execute(array(
         'numeroCommande' => $_SESSION['numCommande']
-    ));
+      ));
+
+    mail($mailUser, $objet, $contenu, $headers);
+
 }
 ?>
