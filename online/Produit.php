3e59fbaaa6637d41f7cf6f8b20cd7281d7e427bf
Tweety63 <32924326+Tweety63@users.noreply.github.com>
2017-11-26 16:36:46 +0100

    JC - 2nd test utf-8

--- online/Produit.php
+++ online/Produit.php
@@ -18,7 +18,7 @@ header('Content-Type: text/html; charset=utf-8');
   	<script type="application/javascript" src="./js/bootstrap.min.js"></script>
     <meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv= »Content-Type » content= »text/html; charset=utf-8″ />
+    <meta http-equiv= "Content-Type" content= "text/html; charset=utf-8"/>
 
   	<title>U POP</title>
   </head>
